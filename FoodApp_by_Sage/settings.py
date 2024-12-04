4ce2dc7c36bcf0537589674634bbec56412e69ed
saksham <x23266724@student.ncirl.ie>
2024-12-04 23:42:41 +0000

    Fix static files configuration

--- FoodApp_by_Sage/settings.py
+++ FoodApp_by_Sage/settings.py
@@ -127,10 +127,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+# Add to your settings.py
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add mime type handling
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
