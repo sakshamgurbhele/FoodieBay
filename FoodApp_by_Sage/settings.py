064e8d0a6f51e8af9c14938929cbe3208aa72ab2
saksham <x23266724@student.ncirl.ie>
2024-12-05 00:51:05 +0000

    Updated static files configuration eb

--- FoodApp_by_Sage/settings.py
+++ FoodApp_by_Sage/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# Add this for better static file handling
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# Disable hashing
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
